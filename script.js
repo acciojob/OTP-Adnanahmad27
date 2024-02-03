d944a7287a8fdee5355216830e83eb48a7d66e0b
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-03 20:50:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+const container = document.querySelector(".code-container");
+let lastEnteredInputElemet = null;
+
+function onEnter(e) {
+	if(e.target.value){
+		e.target.nextElementSibling && e.target.nextElementSibling.focus();
+	}
+}
+function onBackspace(e) {
+	e.target.value = "";
+	e.target.previousElementSibling && e.target.previousElementSibling.focus();
+}
+
+function onInput(e){
+	const currEle = e.target;
+	if(currEle.value){
+		lastEnteredInputElemet = currEle;
+		currEle.nextElementSibling && currEle.nextElementSibling.focus();
+	}
+}
+function onKeyUp(e) {
+	if(e.key === "Enter"){
+		onEnter(e);
+	}else if(e.key ==="Backspace"){
+		onBackspace(e);
+	}
+}
+
+
+for(let i=0 ; i<6 ; i++){
+	const code_continer = document.createElement("input");
+	code_continer.className = "code";
+	code_continer.maxLength = 1;
+	container.appendChild(code_continer);
+	code_continer.addEventListener("input" , onInput);
+	code_continer.addEventListener("keyup" , onKeyUp);
+}
+
